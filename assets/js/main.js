6c87b08bd7efe5ef3331db859288569d16dac12c
satheeshveera <satheeshv@boodskap.io>
2021-07-11 09:29:36 +0530

    address change

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,29 +220,29 @@
     }
   });
 
-  /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
+  // /**
+  //  * Initiate portfolio lightbox
+  //  */
+  // const portfolioLightbox = GLightbox({
+  //   selector: ".portfolio-lightbox",
+  // });
 
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
+  // /**
+  //  * Portfolio details slider
+  //  */
+  // new Swiper(".portfolio-details-slider", {
+  //   speed: 400,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  // });
 
   /**
    * Animation on scroll
